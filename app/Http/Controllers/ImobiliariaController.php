0fc9c43f321097860dbf04637656f8b1ef910cbc
Felipe Bender <felipe.bender@solluti.com.br>
2023-03-13 02:05:24 -0300

    code linted and fixed

--- app/Http/Controllers/ImobiliariaController.php
+++ app/Http/Controllers/ImobiliariaController.php
@@ -17,11 +17,11 @@ class ImobiliariaController extends Controller
     public function store(Request $request)
     {
         $data = ['nome' => $request->input('nome'),
-            'descricao'=> $request->input('descricao'),
+            'descricao' => $request->input('descricao'),
             'cidade' => $request->input('cidade'),
             'estado' => $request->input('estado'),
             'telefone' => $request->input('telefone'),
-            'email' => $request->input('email')
+            'email' => $request->input('email'),
         ];
 
         Imobiliaria::create($data);
@@ -41,11 +41,11 @@ class ImobiliariaController extends Controller
         $imobiliaria = Imobiliaria::find($id);
 
         $data = ['nome' => $request->input('nome') ?? $imobiliaria->nome,
-            'descricao'=> $request->input('descricao') ?? $imobiliaria->descricao,
+            'descricao' => $request->input('descricao') ?? $imobiliaria->descricao,
             'cidade' => $request->input('cidade') ?? $imobiliaria->cidade,
             'estado' => $request->input('estado') ?? $imobiliaria->estado,
             'telefone' => $request->input('telefone') ?? $imobiliaria->telefone,
-            'email' => $request->input('email') ?? $imobiliaria->email
+            'email' => $request->input('email') ?? $imobiliaria->email,
         ];
 
         Imobiliaria::update($data);
